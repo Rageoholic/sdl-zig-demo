37d526ff37aeb03916904e8349c3f0f30fe946c3
cshenton <charlieshenton@me.com>
2020-02-19 22:39:42 +0000

    Updated zig code and example deps vendor

--- build.zig
+++ build.zig
@@ -7,13 +7,12 @@ pub fn build(b: *Builder) void {
     exe.linkSystemLibrary("SDL2");
     exe.linkSystemLibrary("c");
 
+    exe.addLibPath("deps\\lib");
+    exe.addIncludeDir("deps\\include");
+
     b.default_step.dependOn(&exe.step);
     b.installArtifact(exe);
 
-
     const run = b.step("run", "Run the demo");
-    const run_cmd = b.addCommand(".", b.env_map,
-        [][]const u8{exe.getOutputPath(), });
-    run.dependOn(&run_cmd.step);
-    run_cmd.step.dependOn(&exe.step);
+    run.dependOn(&exe.run().step);
 }
